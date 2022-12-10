6f99825ac3a0d077552b870178410d4aa9821275
forest611 <forest611328@gmail.com>
2022-12-10 22:13:34 +0900

    マップレンダー追加、ビルド確認

--- src/main/java/red/man10/man10market/Man10Market.kt
+++ src/main/java/red/man10/man10market/Man10Market.kt
@@ -1,10 +1,21 @@
 package red.man10.man10market
 
 import org.bukkit.plugin.java.JavaPlugin
+import red.man10.man10market.map.MappRenderer
 
 class Man10Market : JavaPlugin() {
+
+    companion object{
+        lateinit var instance : Man10Market;
+    }
+
+
     override fun onEnable() {
         // Plugin startup logic
+
+        instance = this
+
+        MappRenderer.setup(this)
     }
 
     override fun onDisable() {
---
+++ src/main/java/red/man10/man10market/map/MappApp.java
@@ -0,0 +1,22 @@
+package red.man10.man10market.map;
+
+public class MappApp {
+
+    ////////////////////////////////////////////
+    //      App name (must be unique key)
+    //      アプリ名：ユニークな必要があります
+    static String appName = "yourapp";
+
+    ////////////////////////////////////////////
+    //     Draw refresh Cycle:描画割り込み周期
+    //     appTickCycle = 1 -> 1/20 sec
+    static int  drawRefreshCycle = 20;
+
+    static public void register(){}
+
+    static void showCursor(){
+
+    }
+
+
+}
