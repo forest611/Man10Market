a4429f4735904fcbbd8bd79459125560f20f8573
forest611 <forest611328@gmail.com>
2022-12-31 17:58:35 +0900

    価格変化があった時のみマップを更新

--- src/main/java/red/man10/man10market/map/PriceMap.java
+++ src/main/java/red/man10/man10market/map/PriceMap.java
@@ -3,15 +3,20 @@ package red.man10.man10market.map;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import red.man10.man10itembank.ItemBankAPI;
 import red.man10.man10market.Man10Market;
 import red.man10.man10market.Market;
 import red.man10.man10market.Util;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PriceMap {
 
+
+    private static final Map<String, Market.PriceData> priceCache = new HashMap<>();
 
     public static void getPriceMap(Player p, String item) {
 
@@ -34,10 +39,7 @@ public class PriceMap {
             List<String> items = Market.INSTANCE.getItemIndex();
 
             for (String item : items) {
-                MappRenderer.draw("price:" + item, 600, (String key, int mapId, Graphics2D g) -> {
-                    drawPrice(g, item);
-                    return true;
-                });
+                MappRenderer.draw("price:" + item, 600, (String key, int mapId, Graphics2D g) -> drawPrice(g, item));
 
                 MappRenderer.displayTouchEvent("price:" + item, (key, mapId, player, x, y) -> {
                     player.performCommand("mce price " + item);
@@ -56,12 +58,22 @@ public class PriceMap {
 
 
     //      現在値を表示
-    static void drawPrice(Graphics2D g, String item) {
+    static boolean drawPrice(Graphics2D g, String item) {
+
+        Market.PriceData cache = priceCache.get(item);
+        Market.PriceData price = Market.INSTANCE.getPrice(item);
+
+        //価格変化がなかった場合は、更新をしない
+        if (cache != null && cache.getPrice() == price.getPrice()){
+            return false;
+        }
 
         g.setColor(Color.GRAY);
         g.fillRect(0, 0, 128, 128);
 
-        MappDraw.drawImage(g,item,64,20,64,64);
+        int id = ItemBankAPI.INSTANCE.getItemData(item).getId();
+
+        MappDraw.drawImage(g,String.valueOf(id),64,20,64,64);
 
         g.setColor(Color.WHITE);
 
@@ -78,7 +90,6 @@ public class PriceMap {
 
         Color col = Color.YELLOW;
 
-        Market.PriceData price = Market.INSTANCE.getPrice(item);
 
         String strPrice = Util.INSTANCE.format(price.getPrice(), 0);
 
@@ -113,8 +124,7 @@ public class PriceMap {
             g.drawString("売:" + Util.INSTANCE.format(price.getBid(), 0), 4, 98);
         }
 
-
-//        drawGauge(g,item.sell,item.buy);
+        return true;
 
     }
 
